d911b79cdfbede0383e5e9d7bd9587b39aaa4934
youngjun.jo <youngjun.jo@nhn.com>
2019-11-07 17:35:57 +0900

    한글제목테스트

--- site/conf.py
+++ site/conf.py
@@ -57,7 +57,7 @@ source_encoding = 'utf-8'
 master_doc = 'index'
 
 # General information about the project.
-project = 'English title'
+project = u'한글제목'
 copyright = 'NHN Corp. All rights reserved.'
 author = ''
 
